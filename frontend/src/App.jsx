2a0f646f7d332ff40bb276d4b2d59ddf9846199a
Md Arif <aatif149209@gmail.com>
2023-08-01 14:25:18 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
 import axios from "axios";
+import Success from "./Success";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/" element={<Login />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/register" element={<Register />}></Route>
+          <Route path="/home" element={<Success />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
